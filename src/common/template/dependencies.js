d8bd6ecaa5b066f3a57ce8ca495744769e89a686
Diego Dulval <deigodifreitas@gmail.com>
2017-07-04 18:49:39 -0300

    Configuração inicial do Projeto #First Commit

---
+++ src/common/template/dependencies.js
@@ -0,0 +1,19 @@
+import jquery from 'jquery'
+    window.$ = window.jQuery = jquery
+
+require( 'jquery-ui')
+require( 'admin-lte/plugins/fastclick/fastclick.min')
+require('admin-lte/plugins/slimScroll/jquery.slimscroll.min')
+require('admin-lte/dist/js/app.min')
+
+require( 'admin-lte/bootstrap/css/bootstrap.min.css')
+require( 'admin-lte/dist/css/AdminLTE.min.css')
+require( 'admin-lte/dist/css/skins/_all-skins.css')
+require( 'admin-lte/plugins/iCheck/flat/blue.css')
+require( 'font-awesome/css/font-awesome.min.css')
+require( 'ionicons/dist/css/ionicons.min.css')
+
+require( './custom.css')
+
+
+
---
+++ src/main/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import '../common/template/dependencies'
+import Header from '../common/template/Header'
+
+class App extends Component {
+  render() {
+    return (
+      <div className='wrapper'>
+        <Header />
+      </div>
+    )
+  }
+}
+
+export default App
