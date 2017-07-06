729ad5ffcc535eed927d583541fe4f5efdcc974d
Diego Dulval <deigodifreitas@gmail.com>
2017-07-06 15:38:47 -0300

    Integração do componentes MenuTree com o redux

--- src/common/template/menu/MenuTree.jsx
+++ src/common/template/menu/MenuTree.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
-export default props => (
-    <li className='treeview'>
-        <a href>
-            <i className={`fa fa-${props.icon}`}></i> <span>{props.label}</span>
-            <i className='fa fa-angle-left pull-right'></i>
-        </a>
-        <ul className='treeview-menu'>
-            {props.children}
-        </ul>
-    </li>
-)
+import { selectMenuItem } from './menuActions'
+
+class MenuTree extends Component {
+    render() {
+        const selected = this.props.menu.selected === this.props.target
+        return (
+            <li onClick={() => this.props.selectMenuItem(this.props.target)} className={selected ? 'active' : '' + 'treeview'} >
+                <a href>
+                    <i className={`fa fa-${this.props.icon}`}></i> <span>{this.props.label}</span>
+                    <i className='fa fa-angle-left pull-right'></i>
+                </a>
+                <ul className='treeview-menu'>
+                    {this.props.children}
+                </ul>
+            </li>
+        )
+    }
+}
+
+const mapStateToProps = state => ({ menu: state.menu })
+const mapDispatchToProps = dispatch => bindActionCreators({ selectMenuItem }, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(MenuTree)
