2e59f354eae7cfd4867bd42544e92a9391bb67f8
diegodifreitas <diegodifreitas@gmail.com>
2017-07-10 17:44:30 -0300

    Criação do component de Label and Input do Formulario

--- src/admProcess/AdmProcessForm.jsx
+++ src/admProcess/AdmProcessForm.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
 
+import LabelAndInput from '../common/form/LabelAndInput'
+
 class AdmProcessForm extends Component {
 
     render() {
         const { handleSubmit } = this.props
         return (
-            <form role='form' onSubmit= { handleSubmit }>
+            <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
-                    <Field name='prtp' component='input'/>
-                    <Field name='modalidade' component='input'/>
-                    <Field name='descricaoSumaria' component='input'/>
-                    <Field name='pendencias' component='input'/>
+                    <Field name='prtp' component={LabelAndInput}
+                        label='PRTP' cols='12 4' placeholder='Informe o PRTP' />
+                    <Field name='descricaoSumaria' component={LabelAndInput}
+                        label='Descricão Súmaria' cols='12 8' placeholder='Informe a descrição súmaria' />
+                    <Field name='modalidade' component={LabelAndInput}
+                        label='Modalidade' cols='12 6' placeholder='Informe a modalidade' />
+                    <Field name='pendencias' component={LabelAndInput}
+                        label='Pêndencias' cols='12 6' placeholder='Informe se o processo possui pendências' />
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className='btn btn-primary'> Submit </button>
@@ -21,4 +27,4 @@ class AdmProcessForm extends Component {
     }
 }
 
-export default reduxForm({ form: 'admProcessForm'})(AdmProcessForm)
+export default reduxForm({ form: 'admProcessForm' })(AdmProcessForm)
