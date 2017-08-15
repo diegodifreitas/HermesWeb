7caee6bb2fceb5847461e108cb532cda750c1df1
diegodifreitas <diegodifreitas@gmail.com>
2017-08-15 01:47:58 -0300

    Add authentication

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
 import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
 
@@ -8,6 +7,7 @@ import promise from 'redux-promise'
 import multi from 'redux-multi'
 import thunk from 'redux-thunk'
 
+import Routes from './main/Routes'
 import App from './main/App'
 import reducers from './main/reducers'
 import registerServiceWorker from './registerServiceWorker'
@@ -18,9 +18,7 @@ const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
 const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}>
-        <Router >
-            <App  />
-        </Router>
+        <Routes />
     </Provider>
     , document.getElementById('root'))
 registerServiceWorker()
