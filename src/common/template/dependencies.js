a8c43f4fac9beb899d92edaa901c14d71b2b40fc
diegodifreitas <diegodifreitas@gmail.com>
2017-08-22 18:12:50 -0300

    Remove dependencies

--- src/common/template/dependencies.js
+++ src/common/template/dependencies.js
@@ -1,10 +1,10 @@
+
 import jquery from 'jquery'
     window.$ = window.jQuery = jquery
 
 require( 'jquery-ui')
 require( 'admin-lte/bootstrap/js/bootstrap.min')
 require( 'jquery.inputmask/dist/jquery.inputmask.bundle')
-require( 'admin-lte/plugins/fastclick/fastclick.min')
 
 require('bootstrap-datepicker/dist/js/bootstrap-datepicker.min')
 require('bootstrap-datepicker/dist/locales/bootstrap-datepicker.pt-BR.min')
@@ -14,9 +14,8 @@ require('admin-lte/plugins/slimScroll/jquery.slimscroll.min')
 require('admin-lte/dist/js/app.min')
 
 require( 'admin-lte/bootstrap/css/bootstrap.min.css')
-require( 'admin-lte/dist/css/AdminLTE.min.css')
-require( 'admin-lte/dist/css/skins/_all-skins.css')
-require( 'admin-lte/plugins/iCheck/flat/blue.css')
+
+
 require( 'font-awesome/css/font-awesome.min.css')
 require( 'ionicons/dist/css/ionicons.min.css')
 
