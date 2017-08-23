aa7628867e6a035285fede6c9d35211cf812376d
diegodifreitas <diegodifreitas@gmail.com>
2017-08-22 23:39:32 -0300

    Added reducer modal in reducers

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import { reducer as toastrReducer } from 'react-redux-toastr'
 
+import ModalReducer from '../userManagement/modalReducer'
 import TabReducer from '../common/tabs/tabReducer'
 import AdmProcessReducer from '../admProcess/admProcessReducer'
 import MonitoringProcessReducer from '../admProcess/monitoring/monitoringProcessReducer'
@@ -12,6 +13,7 @@ import AuthReducer from '../auth/authReducer'
 
 const rootReducer = combineReducers({
     tab: TabReducer,
+    modal: ModalReducer,
     admProcess: AdmProcessReducer,
     form: formReducer,
     toastr: toastrReducer,
