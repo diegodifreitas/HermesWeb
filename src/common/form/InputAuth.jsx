2d632da472ae0950e458a96c6e40f7ffc977a35a
diegodifreitas <diegodifreitas@gmail.com>
2017-09-09 00:37:31 -0300

    Added error

--- src/common/form/InputAuth.jsx
+++ src/common/form/InputAuth.jsx
@@ -11,6 +11,10 @@ export default props => (
                 type={props.type} />
             <span className={`glyphicon glyphicon-${props.icon}
                 form-control-feedback`}></span>
+            {props.meta.touched &&
+                ((props.meta.error &&
+                    <label className="control-label help-block" for="inputError"><i className="fa fa-times-circle-o"></i> {/* &nbsp; */} {props.meta.error}</label>) ||
+                    (props.meta.warning && <span>{props.meta.warning}</span>))}
         </div>
     </If>
 )
