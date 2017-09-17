6e9adc7b8097d08b1f702dc18dc52d47580c6ede
diegodulval <diegodifreitas@gmail.com>
2017-09-17 13:54:35 -0300

    Create PublicData component

---
+++ src/public/publicData/AdmProcessList.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { Link } from 'react-router-dom'
+
+
+class AdmProcessList extends Component {
+
+    renderRows() {
+        const list = this.props.list || []
+        return list.map(ap => (
+            <tr key={ap.id}>
+                <td> {ap.prtp} </td>
+                <td> {ap.modalidade.nome}  &nbsp; <b>nº:</b> &nbsp; {ap.modalidade.numero} </td>
+                <td> {ap.descricaoSumaria} </td>
+                <td> {ap.pendencias} </td>
+                <td>
+                    <Link className="btn btn-success" to={'/admProcess/' + ap.id + '/monitoring'} >
+                        <i className='fa fa-briefcase' />
+                    </Link>
+                </td>
+            </tr>
+        ))
+    }
+
+    render() {
+        return (
+            <div className='class="box-body table-responsive no-padding"'>
+                <table className='table table-hover'>
+                    <thead>
+                        <tr>
+                            <th> PRTP </th>
+                            <th> Modalidade </th>
+                            <th> Descrição Sumária </th>
+                            <th> Pendências </th>
+                            <th className='table-action'> Ações </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.renderRows()}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default AdmProcessList;
---
+++ src/public/publicData/PublicData.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import { toastr } from 'react-redux-toastr'
+
+import ContentHeader from '../../common/template/ContentHeader'
+import Content from '../../common/template/Content'
+import Box from '../../common/template/box/Box'
+import BoxHeader from '../../common/template/box/BoxHeader'
+import BoxBody from '../../common/template/box/BoxBody'
+import BoxFooter from '../../common/template/box/BoxFooter'
+
+
+import Api from '../../main/api'
+
+import AdmProcessList from './AdmProcessList'
+
+class PublicData extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isLoading: false,
+            list: [],
+            text: ''
+        }
+
+        this.refresh()
+    }
+
+    refresh() {
+        this.setState({ ...this.state, isLoading: true })
+        Api.getAdmProcess()
+            .then(res => {
+                this.setState({ ...this.state, text: '', list: res.data, isLoading: false })
+            })
+            .catch(e => {
+                toastr.error('Erro', 'Não foi acessar o servidor!')
+                this.setState({ ...this.state, text: '', isLoading: false })
+            })
+    }
+
+    render() {
+        return (
+            <div className=''>
+                <ContentHeader title='Dados Publicos' small='1.0' />
+                <Content >
+                    <Box color='primary direct-chat direct-chat-primary'>
+                        {this.state.isLoading &&
+                            <div className="overlay">
+                                <i className="fa fa-refresh fa-spin"></i>
+                            </div>
+                        }
+                        <BoxHeader title='Processo Administrativo' />
+                        <BoxBody>
+                            <AdmProcessList list={this.state.list} />
+                        </BoxBody>
+                    </Box>
+                </  Content>
+            </div>
+        )
+    }
+}
+
+export default PublicData
