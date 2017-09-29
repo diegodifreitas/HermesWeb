65ad9fc4062545f4c5fcb143f5fe77e99019ee30
diegodifreitas <diegodifreitas@gmail.com>
2017-09-29 18:10:30 -0300

    Update user management reducer

--- src/userManagement/userManagementReducer.js
+++ src/userManagement/userManagementReducer.js
@@ -3,7 +3,7 @@ const INITIAL_STATE = { list: [] }
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case "USERS_FETCHED":
-            return { ...state, list: action.payload.data }
+            return { ...state, list: action.payload.data.payload }
         default:
             return state
     }
