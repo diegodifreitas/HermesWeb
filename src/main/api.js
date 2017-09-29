7522381f573f6d7b776cd4a8459855f14719311e
diegodifreitas <diegodifreitas@gmail.com>
2017-09-29 18:09:42 -0300

    Alter API url

--- src/main/api.js
+++ src/main/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/'
+  baseURL: 'http://localhost:8084/HS_WEB/'
 })
 
 export const postAdmProcess = (admProcess) => api.post('admprocess', admProcess)
