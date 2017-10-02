3d60c5e77286e21f5052009c6db86d0e64b17201
diegodulval <diegodifreitas@gmail.com>
2017-10-01 23:47:06 -0300

    Update OSC reducer

--- src/osc/oscReducer.js
+++ src/osc/oscReducer.js
@@ -3,11 +3,7 @@ const INITIAL_STATE = { list: [], qtd: 0 }
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case "OSC_FETCHED":
-            if (action.payload.data.payload) {
-                return { ...state, list: action.payload.data.payload, qtd: action.payload.data.quantity }
-            }else {
-                return state
-            }
+            return { ...state, list: action.payload.data.payload, qtd: action.payload.data.quantity }
         default:
             return state
     }
