5a8e11da2a8d044fbb4b94b5870f7b4036dbc101
diegodifreitas <diegodifreitas@gmail.com>
2017-10-04 12:01:20 -0300

    Update const

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
 export default {
-    API_URL: 'http://localhost:8084/HS_WEB/api',
-    OAPI_URL: 'http://localhost:8084/HS_WEB/oapi',
+    API_URL: 'http://localhost:8084/HS_WEB',
+    OAPI_URL: 'http://localhost:8084/HS_WEB',
 } 
