4efc085e79a59dafac9baeb3efece9e3e787413c
diegodulval <diegodifreitas@gmail.com>
2017-10-07 19:58:08 -0300

    Update ResponsavelForm

--- src/public/ResponsavelForm.jsx
+++ src/public/ResponsavelForm.jsx
@@ -7,47 +7,61 @@ import { init } from './registerActions'
 
 import LabelAndInput from '../common/form/LabelAndInput'
 import LabelAndDate from '../common/form/LabelAndDate'
+import BoxBody from '../common/template/box/BoxBody'
 
 class ResponsavelForm extends Component {
 
     render() {
         const { handleSubmit, readOnly } = this.props
         return (
-            <div className="form">
-                <legend> Responsavél </legend>
-                <Field name='member.name' component={LabelAndInput} readOnly={readOnly}
-                    label='Nome do Responsavél Legal' cols='12 12' placeholder='Nome da responsável pela organização' />
+            <form onSubmit={handleSubmit} className='form'>
+                <BoxBody>
 
-                <Field name='member.cargo' component={LabelAndInput} readOnly={readOnly}
-                    label='Cargo' cols='12 3' placeholder='Informe o Cargo' />
+                    <Field name='boardMemberList[0].name' component={LabelAndInput} readOnly={readOnly}
+                        label='Nome' cols='12 10' placeholder='Informe um Nome*' />
 
-                <Field name='member.cpf' component={LabelAndInput} readOnly={readOnly}
-                    label='CPF' cols='12 6' placeholder='Informe o CPF do Responsavél' />
+                    <Field name='boardMemberList[0].email' component={LabelAndInput} type="email" label='Email'
+                        cols='12 6' readOnly={readOnly} placeholder='Informe um email*' />
 
-                <Field name='member.rg' component={LabelAndInput} label='RG'
-                    cols='12 3' readOnly={readOnly} placeholder='Informe o RG do Responsavél' />
+                    <Field name='boardMemberList[0].phone' component={LabelAndInput} label='Telefone'
+                        cols='12 6' readOnly={readOnly} placeholder='Informe um número de telefone' />
 
-                <Field name='member.street' component={LabelAndInput} readOnly={readOnly}
-                    label='Endereço' cols='12 6' placeholder='Ex: Av. João de Camargo, 89' />
+                    <Field name='boardMemberList[0].cpf' component={LabelAndInput} label='CPF'
+                        cols='12 6' readOnly={readOnly} placeholder='Ex. 126.845.658-61' />
 
-                <Field name='member.neighborhood' component={LabelAndInput} label='Bairro'
-                    cols='12 3' readOnly={readOnly} placeholder='Informe o bairro da localização da organização' />
+                    <Field name='boardMemberList[0].rg' component={LabelAndInput} label='RG'
+                        cols='12 6' readOnly={readOnly} placeholder='Ex. 15.754.580' />
 
-                <Field name='member.phone' component={LabelAndInput} label='Telefone'
-                    cols='12 3' readOnly={readOnly} placeholder='Informe um número de telefone' />
+                    <Field name='boardMemberList[0].office' component={LabelAndInput} label='Cargo'
+                        cols='12 4' readOnly={readOnly} placeholder='Qual seu cargo?' />
 
-                <Field name='member.email' component={LabelAndInput} label='Email'
-                    cols='12 4' readOnly={readOnly} placeholder='Informe o email do Responsavél' type='email' />
+                    <Field name='boardMemberList[0].beginningOfMandate' component={LabelAndDate} label='Início do Mandato'
+                        cols='12 4' readOnly={readOnly} placeholder='Informe a data de início do mandato' type='text' />
 
-                <Field name='member.inicioDoMandato' component={LabelAndDate} label='Início do Mandato'
-                    cols='12 4' readOnly={readOnly} placeholder='Informe a data de início do mandato' type='text' />
+                    <Field name='boardMemberList[0].endOfMandate' component={LabelAndDate} label='Término do Mandato'
+                        cols='12 4' readOnly={readOnly} placeholder='Informe a data de término do mandato' type='text' />
 
-                <Field name='member.terminoDoMandato' component={LabelAndDate} label='Término do Mandato'
-                    cols='12 4' readOnly={readOnly} placeholder='Informe a data de término do mandato' type='text' />
+                </BoxBody>
+                <BoxBody>
+                    <fieldset>
+                        <legend> Localização </legend>
+                        <Field name='boardMemberList[0].address.street' component={LabelAndInput} readOnly={readOnly}
+                            label='Endereço' cols='12 6' placeholder='Ex: Av. João de Camargo' />
 
-                <button type='submit' onClick={handleSubmit} className={`btn btn-${this.props.submitClass}`}> {this.props.submitLabel} </button>
+                        <Field name='boardMemberList[0].address.number' component={LabelAndInput} readOnly={readOnly}
+                            label='Numero' cols='12 6' placeholder='Ex: 456' />
+
+                        <Field name='boardMemberList[0].address.neighborhood' component={LabelAndInput} label='Bairro'
+                            cols='12 6' readOnly={readOnly} placeholder='Ex: Eletrônica' />
+
+                        <Field name='boardMemberList[0].address.city' component={LabelAndInput} readOnly={readOnly}
+                            label='Município' cols='12 6' placeholder='Ex: Santa Rita do Sapucai - MG' />
+                    </fieldset>
+                </BoxBody>
+
+                <button type='submit' className={`btn btn-${this.props.submitClass}`}> {this.props.submitLabel} </button>
                 <button type='button' className='btn btn-default' onClick={this.props.init}> Cancelar </button>
-            </div>
+            </form>
         )
     }
 }
