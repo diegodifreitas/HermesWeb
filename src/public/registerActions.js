ddb1b442abcc8b4587a9a53cca41cdbf0ccc2979
diegodulval <diegodifreitas@gmail.com>
2017-10-07 19:52:49 -0300

    Update Action of post OSC

--- src/public/registerActions.js
+++ src/public/registerActions.js
@@ -1,12 +1,38 @@
 import { toastr } from 'react-redux-toastr'
 import { initialize } from 'redux-form'
-
+import moment from 'moment'
 import Api from '../main/api'
 
 const INITIAL_VALUE = {}
 
 export const create = (values) => {
-    return submit(values, 'postOsc')
+    let data = { ...values }
+
+    if (data.hasOwnProperty("boardMemberList")) {
+        data.boardMemberList = data.boardMemberList.map(x => {
+
+            let begging = moment(x.beginningOfMandate, "DD/MM/YYYY").format("MM/DD/YYYY")
+            let end = moment(x.endOfMandate, "DD/MM/YYYY").format("MM/DD/YYYY")
+
+            if (begging < end) {
+                return {
+                    ...x,
+                    beginningOfMandate: moment(begging).format(),
+                    endOfMandate: moment(end).format(),
+                    responsible: true
+                }
+            } else {
+                toastr.error('Erro', "A data de inicio deve ser anterior a do término do mandato")
+                throw "A data de inicio deve ser anterior a do término do mandato"
+                return null
+            }
+        })
+    } else {
+        toastr.error('Erro', "Preencha as informações do Responsável")
+        throw "Preencha as informações do Responsável"
+    }
+
+    return submit(data, 'postOsc')
 }
 
 const submit = (values, method) => {
@@ -14,12 +40,13 @@ const submit = (values, method) => {
         Api[method](values)
             .then(resp => {
                 toastr.success('Sucesso', 'Operação realizada com sucesso!')
+                window.location.href = 'http://localhost:3000';
                 dispatch(init())
             })
             .catch(e => {
+                window.location.href = 'http://localhost:3000';
                 //alterar isso quando integrar com a API
-                toastr.error('Erro', 'Fica ativo ai, ta dando erro!')
-                //e.response.data.errors.forEach( error => toastr.error('Erro', error))           
+                e.response.data.errors.forEach(error => toastr.error('Erro', error))
             })
     }
 }
