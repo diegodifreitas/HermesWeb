d6626af12b3e45a5f88476da15a503941d3e89bd
diegodifreitas <diegodifreitas@gmail.com>
2017-10-11 17:36:09 -0300

    Mapped member route

--- src/main/routes/OSCRoutes.jsx
+++ src/main/routes/OSCRoutes.jsx
@@ -7,14 +7,16 @@ import AdmProcess from '../../admProcess/AdmProcess'
 import Monitoring from '../../admProcess/monitoring/MonitoringProcess'
 import Dashboard from '../../dashboard/Dashboard'
 import Osc from '../../osc/Osc'
+import Member from '../../osc/member/Member'
 import UserManagement from '../../userManagement/UserManagement'
 
 export default props => (
 
-        <Switch>
-            <Route exact path='/' component={Dashboard} onUpdater={mapActiveUrlToMenu()} />
-            <Route exact path='/myAccount' component={AccountInfo} />
-            <Route exact path='/admProcess' component={AdmProcess} />
-            <Route path='*' component={Dashboard} />
-        </Switch>
+    <Switch>
+        <Route exact path='/' component={Dashboard} onUpdater={mapActiveUrlToMenu()} />
+        <Route exact path='/myAccount' component={AccountInfo} />
+        <Route exact path='/admProcess' component={AdmProcess} />
+        <Route exact path='/members' component={Member} />
+        <Route path='*' component={Dashboard} />
+    </Switch>
 )
