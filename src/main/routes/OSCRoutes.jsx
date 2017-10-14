fde62950c8ad2b9f4563b805451c7ea55327ce99
diegodulval <diegodifreitas@gmail.com>
2017-10-14 00:03:10 -0300

    Update Routes

--- src/main/routes/OSCRoutes.jsx
+++ src/main/routes/OSCRoutes.jsx
@@ -4,18 +4,17 @@ import { Route, Switch } from 'react-router-dom'
 import { mapActiveUrlToMenu } from '../../common/template/menu/menuActiveClass'
 import AccountInfo from '../../accountInfo/AccountInfo'
 import AdmProcess from '../../admProcess/AdmProcess'
-import Monitoring from '../../admProcess/monitoring/MonitoringProcess'
 import Dashboard from '../../dashboard/Dashboard'
-import Osc from '../../osc/Osc'
 import Member from '../../osc/member/Member'
-import UserManagement from '../../userManagement/UserManagement'
 
 export default props => (
 
     <Switch>
         <Route exact path='/' component={Dashboard} onUpdater={mapActiveUrlToMenu()} />
         <Route exact path='/myAccount' component={AccountInfo} />
+        {props.approval &&
         <Route exact path='/admProcess' component={AdmProcess} />
+        }
         <Route exact path='/members' component={Member} />
         <Route path='*' component={Dashboard} />
     </Switch>
