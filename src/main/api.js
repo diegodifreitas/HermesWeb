7d0c597874528e25bf65cde0cc98435bcf7c06b9
diegodulval <diegodifreitas@gmail.com>
2017-11-09 22:11:30 -0200

    Update endpoints api

--- src/main/api.js
+++ src/main/api.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: 'http://localhost:8080',
-  headers: { 'Access-Control-Expose-Headers': 'Authentication'}
 })
 
 export const postAdmProcess = (admProcess) => api.post('admprocess', admProcess)
