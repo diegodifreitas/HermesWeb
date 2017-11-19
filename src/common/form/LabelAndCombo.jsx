e1914b2b1b535f1e0ff4d80d104ade178c4ac01f
diegodulval <diegodifreitas@gmail.com>
2017-11-18 23:33:18 -0200

    Update common Form components

--- src/common/form/LabelAndCombo.jsx
+++ src/common/form/LabelAndCombo.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import Grid from '../layout/Grid'
- 
+
 export default class LabelAndCombo extends Component {
     renderOptions() {
-        const empty = {id: null, value:null, nome: 'Selecione'}
+        const empty = { id: null, value: null, nome: 'Selecione' }
         const values = this.props.values ? [empty, ...this.props.values] : [empty]
         return values.map(v => (
             <option key={v.id} value={v.value}>{v.nome}</option>
@@ -13,12 +13,16 @@ export default class LabelAndCombo extends Component {
         const { props } = this
         return (
             <Grid cols={props.cols}>
-                <div className='form-group'>
+                <div className={props.meta.touched && props.meta.error ? 'form-group has-error' : 'form-group'}>
                     <label>{props.label}</label>
-                    <select className="form-control" {...props.input} 
+                    <select className="form-control" {...props.input}
                         readOnly={props.readOnly} type={props.type} ref="select">
                         {this.renderOptions()}
                     </select>
+                    {props.meta.touched &&
+                        ((props.meta.error &&
+                            <label className="control-label help-block" htmlFor="inputError"><i className="fa fa-times-circle-o"></i> {/* &nbsp; */} {props.meta.error}</label>) ||
+                            (props.meta.warning && <span>{props.meta.warning}</span>))}
                 </div>
             </Grid>
         )
--- src/common/form/LabelAndTextArea.jsx
+++ src/common/form/LabelAndTextArea.jsx
@@ -3,13 +3,17 @@ import Grid from '../layout/Grid'
 
 export default props => (
     <Grid cols={props.cols}>
-        <div className='form-group'>
+        <div className={props.meta.touched && props.meta.error ? 'form-group has-error' : 'form-group'}>
             <label htmlFor={props.name}> {props.label} </label>
-            <textarea {...props.input} 
-                    className={`form-control ${props.style}`}
-                    placeholder={props.placeholder} 
-                    readOnly={props.readOnly} 
-                    type={props.type} />
+            <textarea {...props.input}
+                className={`form-control ${props.style}`}
+                placeholder={props.placeholder}
+                readOnly={props.readOnly}
+                type={props.type} />
+            {props.meta.touched &&
+                ((props.meta.error &&
+                    <label className="control-label help-block" htmlFor="inputError"><i className="fa fa-times-circle-o"></i> {/* &nbsp; */} {props.meta.error}</label>) ||
+                    (props.meta.warning && <span>{props.meta.warning}</span>))}
         </div>
     </Grid>
 )
