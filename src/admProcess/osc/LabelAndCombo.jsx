06bf48f8ece17b839fe672d864c21c9e8b2bb205
diegodulval <diegodifreitas@gmail.com>
2017-11-18 23:33:56 -0200

    Added osc in AdmProcess

---
+++ src/admProcess/osc/LabelAndCombo.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import Grid from '../../common/layout/Grid'
+
+export default class LabelAndCombo extends Component {
+    renderOptions() {
+        const empty = { id: 0, value: null, name: 'Selecione' }
+        const values = this.props.values ? [empty, ...this.props.values] : [empty]
+        return values.map(v => (
+            <option key={v.id} value={v.id}>{v.name}</option>
+        ))
+    }
+    render() {
+        const { props } = this
+        return (
+            <Grid cols={props.cols}>
+                <div className={props.meta.touched && props.meta.error ? 'form-group has-error' : 'form-group'}>
+                    <label>{props.label}</label>
+                    <select className="form-control" {...props.input}
+                        readOnly={props.readOnly} type={props.type} ref="select">
+                        {this.renderOptions()}
+                    </select>
+                    {props.meta.touched &&
+                        ((props.meta.error &&
+                            <label className="control-label help-block" htmlFor="inputError"><i className="fa fa-times-circle-o"></i> {/* &nbsp; */} {props.meta.error}</label>) ||
+                            (props.meta.warning && <span>{props.meta.warning}</span>))}
+                </div>
+            </Grid>
+        )
+    }
+}
