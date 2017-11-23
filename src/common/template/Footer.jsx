be9ebdc037866fa81c966494891047ef4b863045
diegodulval <diegodifreitas@gmail.com>
2017-11-23 00:34:25 -0200

    Updated Footer component

--- src/common/template/Footer.jsx
+++ src/common/template/Footer.jsx
@@ -5,8 +5,9 @@ export default props => (
         <div className='pull-right hidden-xs'>
             <b>Version</b> 1.0
         </div>
-        <strong> Copyrigth &copy; 2017
+        <strong> Direitos reservados a 
             <a href='https://github.com/diegodifreitas' target='noopener'> Hermes </a>
+            &copy; 2017
         </strong>
     </footer>
 )
